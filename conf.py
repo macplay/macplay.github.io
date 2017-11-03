a495eeaa35881f520c04fb70a51520114dfabaa7
ashfinal <ashfinal@gmail.com>
2017-11-04 07:21:47 +0800

    style improvement

--- conf.py
+++ conf.py
@@ -1310,7 +1310,7 @@ ADDITIONAL_METADATA = {"category": "", "previewimage": "", "nocomments": "",
 # If webassets is installed, bundle JS and CSS into single files to make
 # site loading faster in a HTTP/1.1 environment but is not recommended for
 # HTTP/2.0 when caching is used. Defaults to True.
-USE_BUNDLES = True
+USE_BUNDLES = False
 
 # Plugins you don't want to use. Be careful :-)
 # DISABLED_PLUGINS = ["render_galleries"]
